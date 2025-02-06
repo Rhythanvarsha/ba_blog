894af2623d58bc2cf0d51cb63fcd7a8c23168931
RHYTHAN VARSHA JANAKIRAM <152013507+Rhythanvarsha@users.noreply.github.com>
2025-02-06 14:05:24 +0530

    Update Index.js

--- Index.js/Index.js
+++ Index.js/Index.js
@@ -115,7 +115,7 @@ app.use(cors({
 
 app.use(express.json());
 
-const dbURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/your_database_name';
+const dbURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/Main_Blog';
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('Could not connect to MongoDB:', err));
